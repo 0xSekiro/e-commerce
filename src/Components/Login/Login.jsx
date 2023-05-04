eadbe02a66b1232c79484a49d7e7d774811c631e
sarah hesham <etshyko@gmail.com>
2023-05-04 14:46:40 +0300

    forgotPassword

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -39,6 +39,50 @@ export default function SignIn({getUserDataDecoded}) {
   }
   }
 
+ function toForgotPassword() {
+    $(".inner-signUp").fadeOut(500,function(){
+
+      setTimeout(() => {
+        $(".forgot").fadeIn(500)
+      }, 1000);
+    })
+  
+     
+  }
+
+
+  async function forgotPass() {
+    try {
+      
+      setisLoading(true)
+      const {data} = await axios.post (`https://e-commerce-9w3i.onrender.com/api/v1/auth/forgotPassword` , {
+      
+    
+      "email": document.querySelector(".forgotEmail").value
+      
+    
+   
+  
+  })
+  if(data.status=="success") {
+    $(".sentDone").fadeIn(500)
+    setisLoading(false)
+  }
+ 
+console.log(data);
+      
+    } catch (error) {
+      console.log("Error",error);
+      setisLoading(false)
+      $(".doesntExist").fadeIn(500,function(){
+        setTimeout(() => {
+          $(".doesntExist").fadeOut(500)
+        }, 500);
+      })
+    }
+    
+  }
+
 let user = {
   "email":"",
   "password":"",
@@ -63,16 +107,19 @@ onSubmit : function (values) {
 
 
 return <>
+<div className="alert alert-danger doesntExist" style={{display:"none"}}>Email doesn't exist !</div>
+<div className="alert alert-success sentDone "  style={{display:"none"}}>Reset Link sent successfully to your email !</div>
 
 <div style={{"textAlign":"center" , "display":"none"}} className='alert alert-danger errorMsg'>Incorrect Email or password</div>
 <div style={{"textAlign":"center" ,"display":"none" }} className='alert alert-success successMsg'>Welcome back !</div>
 
 
-<div className="container d-flex justify-content-center mt-5 pt-5">
-    <div className="inner-signUp w-50">
+<div className="container  justify-content-center mt-5 pt-5 logINS">
+    <div className="inner-signUp w-50 ">
       <div className="row">
       <div className="col-md-12">
         <h3>Login</h3>
+        
         <form onSubmit={formik.handleSubmit}>
         
         <label className='my-2' htmlFor="email">email</label>
@@ -85,6 +132,8 @@ return <>
 
    
         {isLoading? <button  className='btn btn-success ' type='button'><i className="fa-solid fa-spinner fa-spin  mt-3"></i></button>:<button type='submit' className='btn btn-success mt-3 load-bt'>Login</button>}
+        <button onClick={toForgotPassword} type='button' className='float-end btn btn-outline-primary mt-3 '>Forgot Password</button>
+        
         <p className='my-3'>New member ? <Link to="/register">Sign up</Link></p>  
         <button type="button" className="login-with-google-btn my-2" >
   Sign up with Google
@@ -92,15 +141,25 @@ return <>
 
         </form>
         </div>
-     
 
         
       </div>
 
     </div>
 
-</div>
+    <div className=" w-50 m-auto forgot" style={{display:"none"}} >
+         <div className='w-75 text-center'>
+          <h4>Forgot your password?</h4>
+        <p>Enter your email address and we'll send you a link to reset your password</p>
+          </div>
+        <input type="email" className='form-control forgotEmail' placeholder='enter your email'/>
+        {isLoading ? <button  className='w-50 btn btn-outline-primary mt-3 ' type='button'><i className="fa-solid fa-spinner fa-spin  mt-3"></i></button> :         <button onClick={forgotPass} className='w-50 btn btn-outline-primary mt-3 '>Confirm email</button>}
 
+         </div>
+
+        
+
+</div>
 
 </>
 }
