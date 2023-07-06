07e10ff93c1b2f6ff57741655274499cdb4e7fc2
0xSekiro <mohamed0xmuslim@gmail.com>
2023-07-06 06:19:00 +0300

    fix forgot password error and test it

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,8 +2,8 @@ const User = require("../models/userModel");
 const bcrypt = require("bcrypt");
 const crypto = require("crypto");
 const jwt = require("jsonwebtoken");
-const sendEmail = require("../utilities//sendgrid");
 const errHandler = require("../controllers/errorController");
+const transporter = require("../utilities/nodemailer");
 
 function signToken(id) {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -89,73 +89,102 @@ exports.checkAuthorization = (req, res, next) => {
 };
 
 exports.forgotPassword = async (req, res) => {
-  const user = await User.findOne({ email: req.body.email });
-  if (!user) {
-    return errHandler.returnError(400, "Email not found", res);
+  try {
+    if (!req.body.email) {
+      return errHandler.returnError(400, "Missing email field", res);
+    }
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+      return errHandler.returnError(400, "Email not found", res);
+    }
+
+    const generatedToken = crypto.randomBytes(32).toString("hex");
+
+    user.resetToken = crypto
+      .createHash("sha256")
+      .update(generatedToken)
+      .digest("hex");
+    user.expireToken = Date.now() + 1000 * 60 * 10;
+
+    await user.save({ validateBeforeSave: false });
+
+    // send email
+    let msg;
+    if (process.env.ENV == "development") {
+      msg = `Forgot your password? send PATCH request with password and passwordConfirm to https://127.0.0.1:8080/api/v1/auth/resetPassword/${generatedToken} \n( Token valid for 10 min )\n`;
+    } else {
+      msg = `Forgot your password? send GET request to https://sarahheshamali2.github.io/connectedEcommerce/#/reset/${generatedToken} \n( Token valid for 10 min )\n`;
+    }
+
+    await transporter.sendMail({
+      from: "mohamed0xmuslim@gmail.com",
+      to: req.body.email,
+      subject: "Reset password link",
+      html: msg,
+    });
+
+    res.status(200).json({
+      status: "success",
+      message: "token sent to email",
+    });
+  } catch (err) {
+    errHandler.returnError(500, "Something went wrong", res);
   }
-
-  const generatedToken = crypto.randomBytes(32).toString("hex");
-
-  user.resetToken = crypto
-    .createHash("sha256")
-    .update(generatedToken)
-    .digest("hex");
-  user.expireToken = Date.now() + 1000 * 60 * 10;
-
-  await user.save({ validateBeforeSave: false });
-  let msg;
-  if (process.env.ENV == "development") {
-    msg = `Forgot your password? send PATCH request with password and passwordConfirm to https://127.0.0.1:8080/api/v1/auth/resetPassword/${generatedToken} \n( Token valid for 10 min )\n`;
-  } else {
-    msg = `Forgot your password? send GET request to https://sarahheshamali2.github.io/connectedEcommerce/#/reset/${generatedToken} \n( Token valid for 10 min )\n`;
-  }
-
-  await sendEmail(req.body.email, "Password reset token", msg);
-
-  res.status(200).json({
-    status: "success",
-    message: "token sent to email",
-  });
 };
 
 exports.resetPassword = async (req, res) => {
-  const token = crypto
-    .createHash("sha256")
-    .update(req.params.token)
-    .digest("hex");
-  const user = await User.findOne({
-    resetToken: token,
-    expireToken: { $gt: Date.now() },
-  });
-
-  if (!user)
-    return res
-      .status(400)
-      .json({ status: "fail", message: "Invalid token or has expired" });
-
-  if (!req.body.password || !req.body.passwordConfirm) {
-    return errHandler.returnError(
-      400,
-      "Please enter new password and confirm it",
-      res
-    );
-  }
-
-  user.password = req.body.password;
-  user.passwordConfirm = req.body.passwordConfirm;
-  user.resetToken = undefined;
-  user.expireToken = undefined;
-
   try {
-    await user.save();
-  } catch (err) {
-    return errHandler.returnError(500, "Something went wrong", res);
-  }
+    const token = crypto
+      .createHash("sha256")
+      .update(req.params.token)
+      .digest("hex");
+    const user = await User.findOne({
+      resetToken: token,
+      expireToken: { $gt: Date.now() },
+    });
 
-  res.status(200).json({
-    status: "success",
-    message: "password changed successfully",
-  });
+    if (!user)
+      return res
+        .status(400)
+        .json({ status: "fail", message: "Invalid token or has expired" });
+
+    if (!req.body.password || !req.body.passwordConfirm) {
+      return errHandler.returnError(
+        400,
+        "Please enter new password and confirm it",
+        res
+      );
+    }
+    const password = String(req.body.password);
+    const passwordConfirm = String(req.body.passwordConfirm);
+    if (password !== passwordConfirm) {
+      return errHandler.returnError(400, "Passwords are not the same", res);
+    } else if (password.length < 8) {
+      return errHandler.returnError(
+        400,
+        "Password length are less than 8",
+        res
+      );
+    }
+
+    user.password = password;
+    user.passwordConfirm = passwordConfirm;
+    user.resetToken = undefined;
+    user.expireToken = undefined;
+
+    try {
+      await user.save();
+    } catch (err) {
+      return errHandler.returnError(500, "Something went wrong", res);
+    }
+
+    res.status(200).json({
+      status: "success",
+      message: "password changed successfully",
+    });
+  } catch (err) {
+    errHandler.returnError(500, "Something went wrong", res);
+  }
 };
 
 exports.logWithGoogle = (req, res) => {
